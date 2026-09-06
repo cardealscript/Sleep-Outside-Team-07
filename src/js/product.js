73b9ff732ea64f52a792fa8d4cb558045c5faf37
cardealscript <cfelisberto@byupathway.edu>
2026-09-07 00:31:38 +0200

    Fix: Add to cart now supports multiple products

--- src/js/product.js
+++ src/js/product.js
@@ -4,6 +4,8 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
+  let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+  cart.push(product);
   setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
