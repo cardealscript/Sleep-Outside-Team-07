b3ba65ebfddc9186b2b6a05f0720deb58810c823
cardealscript <cfelisberto@byupathway.edu>
2026-09-16 01:52:34 +0200

    Add dynamic product detail page using ProductDetails.mjs, getParam, and ES modules

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,43 @@
+import { setLocalStorage } from './utils.mjs';
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+    document
+      .getElementById('addToCart')
+      .addEventListener('click', this.addProductToCart.bind(this));
+  }
+
+  addProductToCart() {
+    let cart = JSON.parse(localStorage.getItem('so-cart')) || [];
+    cart.push(this.product);
+    setLocalStorage('so-cart', cart);
+  }
+
+  renderProductDetails() {
+    document.querySelector('h3').textContent = this.product.Brand.Name;
+    document.querySelector('h2').textContent = this.product.NameWithoutBrand;
+
+    const productImage = document.querySelector('img');
+    productImage.src = this.product.Image;
+    productImage.alt = this.product.NameWithoutBrand;
+
+    document.querySelector('.product-card__price').textContent =
+      `$${this.product.FinalPrice}`;
+
+    document.querySelector('.product__color').textContent =
+      this.product.Colors[0].ColorName;
+
+    document.querySelector('.product__description').innerHTML =
+      this.product.DescriptionHtmlSimple;
+
+    document.getElementById('addToCart').dataset.id = this.product.Id;
+  }
+}
